334535f14b2d57aad5388fdbd1e9bd67c2603353
Inocea <sugiao1114@gmail.com>
2022-10-24 10:37:54 +0900

    Add ブログ投稿詳細の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,11 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
     }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+     //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+     //dd関数で中身の確認が出来る
+    }
 }
